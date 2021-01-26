3081b7e78698f6253c79c1391cd949fd918051f2
Alicja <pasztelana@gmail.com>
2021-01-26 14:42:38 +0100

    Migrations have been created, entities in database have been created with the usage of migrations

--- database/migrations/2021_01_26_132046_create_archive_table.php
+++ database/migrations/2021_01_26_132046_create_archive_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArchiveTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('archive', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('newest_id')->unsigned();
+            $table->foreign('newest_id')->references('id')->on('newest')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('archive');
     }
 }
